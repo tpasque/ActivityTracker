4e19970d36dee080d15118a75997e85c5c1860ce
Corey Yaspan <cspan00@gmail.com>
2016-03-09 11:17:33 -0700

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,11 @@
+var app = angular.module("myApp", ['ngRoute', 'ngResource']);
+
+
+app.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'MainController'
+      })
+    $locationProvider.html5Mode(true);
+});
