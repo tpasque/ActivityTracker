3898f2fc60bb5fc4c035dec2e671e1289616a43f
Tony McKendry <tonymckendry@gmail.com>
2016-03-15 15:43:38 -0600

    profile page layout created

--- public/js/app.js
+++ public/js/app.js
@@ -32,6 +32,10 @@ app.config(function ($routeProvider, $authProvider, $locationProvider) {
   templateUrl: 'partials/post/new.html',
   controller: 'newController'
 })
+ .when('/profile',{
+  templateUrl: 'partials/profile/show.html',
+  controller: 'mainController'
+})
  $locationProvider.html5Mode(true);
 
 });
